016a21d260cc0d0ed2f6078dddd94e0a1cbabd8a
orucaslan <oruc_aslan@outlook.com>
2023-03-12 20:04:20 +0300

    first commit

---
+++ veri_yukleme.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 19 16:13:30 2023
+
+@author: oruc2
+"""
+
+import pandas as pd 
+import numpy as np
+import matplotlib.pyplot as plt
+
+veriler = pd.read_csv('C:/Users/oruc2/OneDrive/Masaüstü/BTK/Python ile Makine Öğrenmesi/Veri Setleri/veriler.csv')
+
+
+print(veriler)
